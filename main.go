802096777992e31e3db4b16dc25f44424411e2a1
Henri LEPIC <henrilepic@gmail.com>
2022-01-20 11:29:10 +0100

    create todo

--- main.go
+++ main.go
@@ -8,19 +8,12 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/users/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		u, ok := listUser[id]
-		if !ok {
-			c.JSON(http.StatusNotFound, gin.H{
-				"id": id,
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"user": u,
-		})
-	})
+	r.GET("/users/:id", HandlerGetUser)
+	// TODO: implement all routes
+	//r.POST("/users",....)
+	//r.GET("/users",....)
+	//r.DELETE("/users",....)
+
 	r.Run(":8081")
 }
 
@@ -31,4 +24,18 @@ var listUser map[string]*User = map[string]*User{
 type User struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
+}
+
+func HandlerGetUser(c *gin.Context) {
+	id := c.Param("id")
+	u, ok := listUser[id]
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{
+			"id": id,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"user": u,
+	})
 }
