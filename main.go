8c58450899c9c7b701073549b12d340f52a028d4
Henri LEPIC <henrilepic@gmail.com>
2022-01-20 11:02:44 +0100

    add gin.

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8081")
+}
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
