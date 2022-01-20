b6d294e0b8a691aac7945d2231c8672f16ddda2f
Henri LEPIC <henrilepic@gmail.com>
2022-01-20 11:27:02 +0100

    user: create get user

--- main.go
+++ main.go
@@ -8,12 +8,24 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		u, ok := listUser[id]
+		if !ok {
+			c.JSON(http.StatusNotFound, gin.H{
+				"id": id,
+			})
+			return
+		}
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"user": u,
 		})
 	})
 	r.Run(":8081")
+}
+
+var listUser map[string]*User = map[string]*User{
+	"abcd": {FirstName: "Bob", LastName: "Pike"},
 }
 
 type User struct {
