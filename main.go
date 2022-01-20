da9b851afd087b7257ce7649794305a017745a62
Henri LEPIC <henrilepic@gmail.com>
2022-01-20 11:54:37 +0100

    user: add endpoints from todo.

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/users/:id", HandlerGetUser)
-	// TODO: implement all routes
-	//r.POST("/users",....)
-	//r.GET("/users",....)
-	//r.DELETE("/users/:id",....)
-
+	r.POST("/users", HandlerCreateUser)
+	r.GET("/users", HandlerGetAllUser)
+	r.DELETE("/users/:id", HandlerDeleteUser)
 	r.Run(":8081")
 }
 
@@ -22,6 +22,7 @@ var listUser map[string]*User = map[string]*User{
 }
 
 type User struct {
+	ID        string `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 }
@@ -37,5 +38,50 @@ func HandlerGetUser(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"user": u,
+	})
+}
+
+func HandlerGetAllUser(c *gin.Context) {
+	if len(listUser) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": errors.New("no users found"),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"users": listUser,
+	})
+}
+
+func HandlerCreateUser(c *gin.Context) {
+	var u User
+	err := c.BindJSON(&u)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": err,
+		})
+		return
+	}
+
+	u.ID = uuid.New().String()
+	listUser[u.ID] = &u
+
+	c.JSON(http.StatusOK, gin.H{
+		"user": u,
+	})
+}
+
+func HandlerDeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	_, ok := listUser[id]
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{
+			"id": id,
+		})
+		return
+	}
+	delete(listUser, id)
+	c.JSON(http.StatusAccepted, gin.H{
+		"delete": id,
 	})
 }
