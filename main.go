13bcc0f03b0b045e4bd2e2a2e9b05df916925e6f
Henri LEPIC <henrilepic@gmail.com>
2022-02-17 11:18:50 +0100

    jwt: add middleware

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 
 	"github.com/ritoon/estiam/db/moke"
 	"github.com/ritoon/estiam/service"
+	"github.com/ritoon/estiam/util"
 )
 
 func main() {
 	r := gin.Default()
 	db := moke.New()
-	s := service.New(db, []byte("toto"))
+	secretKey := []byte("toto")
+	secureJWT := util.MiddlJWT(secretKey)
+	s := service.New(db, secretKey)
 	r.GET("/users/:id", s.GetUser)
 	r.POST("/users", s.CreateUser)
 	r.GET("/users", s.GetAllUser)
-	r.DELETE("/users/:id", s.DeleteUser)
+	r.DELETE("/users/:id", secureJWT, s.DeleteUser)
 	r.POST("/login", s.Login)
 	r.Run(":8081")
 }
--- util/jwt.go
+++ util/jwt.go
@@ -2,8 +2,11 @@ package util
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
 
@@ -33,4 +36,30 @@ func CreateJWT(signKey []byte, userUUID, firstName string) (string, error) {
 		return "", err
 	}
 	return fmt.Sprintf("%v", ss), nil
+}
+
+func MiddlJWT(signKey []byte) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+
+		jwtValue := ctx.GetHeader("Authorization")
+		if len(jwtValue) == 0 || !strings.Contains(jwtValue, "Bearer ") || len(jwtValue) < 200 {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.Parse(jwtValue[6:], func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return signKey, nil
+		})
+
+		if claims, ok := token.Claims.(MyCustomClaims); ok && token.Valid {
+			fmt.Println(claims.UserUUID, claims.FirstName)
+		} else {
+			fmt.Println(err)
+		}
+
+	}
 }
